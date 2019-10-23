15e0d6be636e3aae3caa7693eb166a43dbf5a129
CezKraken <CezKraken>
2019-10-23 16:18:22 +0200

    map to index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,10 @@
 import "bootstrap";
+
+
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibW9iZWV0cmF2ZWwiLCJhIjoiY2syM2FkamVrMGw2YTNsbXpuY3NwdTVxciJ9.tvaavgmzqsqDjAblGVJ9Kw';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11'
+});
